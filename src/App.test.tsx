dccfcb503722ebd6a2efcc63f5bd1c7d3ad293c9
Thomas Roche <thomas.a.roche@gmail.com>
2022-09-23 09:09:31 +0100

    trying to fix the App test by mocking

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('./components/pages/Page', () => ({
+  Page: () => <mock-page data-testid="page" />,
+}));
+
+test('If App is passed the open prop App is rendered', () => {
+  const { queryByTestId } = render(<App />);
+  expect(queryByTestId('page')).toBe(true);
 });
