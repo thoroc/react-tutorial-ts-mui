1b632ecf15d55205a860fbb454b86d7777a01b27
Thomas Roche <thomas.a.roche@gmail.com>
2022-09-23 16:28:50 +0100

    getting heading by role in GenericTemplate test

--- src/components/templates/GenericTemplate.spec.tsx
+++ src/components/templates/GenericTemplate.spec.tsx
@@ -4,18 +4,22 @@ import GenericTemplate from './GenericTemplate';
 describe('GenericTemplate component', () => {
   describe('When GenericTemplate is rendered', () => {
     it('Should contains a header', () => {
+      // Arrange
       render(
         <GenericTemplate>
           <div>foo</div>
         </GenericTemplate>,
       );
 
-      expect(
-        screen.queryByText(
-          'React Tic-Tac-Toe Tutorial (TypeScript and Material-UI ver.)',
-        ),
-      ).toBeInTheDocument();
-      expect(screen.queryByText('foo')).toBeInTheDocument();
+      // Act
+      const headingElement = screen.getByRole('heading', {
+        name: 'React Tic-Tac-Toe Tutorial (TypeScript and Material-UI ver.)',
+      });
+      const childElement = screen.queryByText(/foo/i);
+
+      // Assert
+      expect(headingElement).toBeInTheDocument();
+      expect(childElement).toBeInTheDocument();
     });
   });
 });
