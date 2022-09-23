43319b1f99ac0b48d5d235dc331459619423736a
Thomas Roche <thomas.a.roche@gmail.com>
2022-09-23 16:19:40 +0100

    using screen in our test of GenericTemplate

--- src/components/templates/GenericTemplate.spec.tsx
+++ src/components/templates/GenericTemplate.spec.tsx
@@ -1,21 +1,21 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import GenericTemplate from './GenericTemplate';
 
 describe('GenericTemplate component', () => {
   describe('When GenericTemplate is rendered', () => {
     it('Should contains a header', () => {
-      const { queryByText } = render(
+      render(
         <GenericTemplate>
           <div>foo</div>
         </GenericTemplate>,
       );
 
       expect(
-        queryByText(
+        screen.queryByText(
           'React Tic-Tac-Toe Tutorial (TypeScript and Material-UI ver.)',
         ),
       ).toBeInTheDocument();
-      expect(queryByText('foo')).toBeInTheDocument();
+      expect(screen.queryByText('foo')).toBeInTheDocument();
     });
   });
 });
